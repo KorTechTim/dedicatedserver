63bb218755bcaa5fc32c71c1a071da53e8367b92
KorTechTim <127585101+KorTechTim@users.noreply.github.com>
2023-03-20 08:35:13 +0900

    Create longvinter_docker_1.sh

---
+++ longvinter_docker_1.sh
@@ -0,0 +1,16 @@
+# 계정 생성 및 권한 설정
+echo "--------------------------------------------------------"
+echo "Tech Tim 채널 구독과 좋아요 부탁 드립니다."
+echo "https://www.youtube.com/channel/UCTKC1JQaVY-QvLKyGt84I5w" 
+echo 
+echo "steamcmd 계정으 생성합니다. 새 패스워드를 넣어 주세요"
+echo "--------------------------------------------------------"
+echo 
+sleep 2
+
+
+sudo useradd -m -d /home/steam steamcmd
+sudo passwd steamcmd
+sudo usermod -aG sudo steamcmd
+sudo chsh -s /bin/bash steamcmd 
+sudo su - steamcmd
