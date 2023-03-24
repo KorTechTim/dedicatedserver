455c5890bffad8758c24aea5fda422954fb451b7
KorTechTim <127585101+KorTechTim@users.noreply.github.com>
2023-03-24 15:52:11 +0900

    Update Mine_Craft_Ver_1.19.4.sh

--- Mine_Craft_Ver_1.19.4.sh
+++ Mine_Craft_Ver_1.19.4.sh
@@ -35,8 +35,6 @@ sudo apt install screen openjdk-18-jdk -y
 
 # 마인크래프트 서버 설치 
 cd ~
-mkdir minecraft_server
-cd ~/minecraft_server
 wget https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar
 
 # 마인크래프트 초기 실행 
@@ -49,14 +47,13 @@ sudo iptables -I INPUT -p tcp --dport 25565 -j ACCEPT
 
 
 ## 라이센스 파일 등록
-cat <<-EOF > ~/minecraft_server/eula.txt
+cat <<-EOF > ~/eula.txt
 eula=true
 EOF
 
 ## 완료 ##
 echo "---------------------------------------------------------------------------"
 echo "설치가 완료 되었습니다 아래 명령어를 사용하여 서버를 실행하세요" 
-echo "cd ~/minecraft_server"
 echo "java -Xmx1G -Xms1G -jar ~/minecraft_server/server.jar nogui"
 echo ""
 echo "옵션 설명 ==> Xmx : 최대 메모리, Xms : 최소 메모리"
