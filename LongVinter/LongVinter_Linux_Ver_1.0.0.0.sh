14540e45cc236c2d9048c8611bb5d7f6588ecfdb
KorTechTim <127585101+KorTechTim@users.noreply.github.com>
2023-04-13 21:51:02 +0900

    Update LongVinter_Linux_Ver_1.0.0.0.sh

--- LongVinter/LongVinter_Linux_Ver_1.0.0.0.sh
+++ LongVinter/LongVinter_Linux_Ver_1.0.0.0.sh
@@ -34,7 +34,8 @@ InstallApps()
   sudo apt update -y && sudo apt upgrade -y && sleep 1  
   sudo apt install git git-lfs -y && sleep 1 
   sudo add-apt-repository multiverse -y && sleep 1
-  sudo dpkg --add-architecture i386 -y && sleep 1
+  sudo dpkg --add-architecture i386 -y 
+  sleep 1 
   sudo apt update -y 
   sudo apt install lib32gcc-s1 steamcmd -y 
 }
