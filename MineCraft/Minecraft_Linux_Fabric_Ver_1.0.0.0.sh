ae9f56f490e9ca65d70373c8bafbd28b9aee5363
KorTechTim <127585101+KorTechTim@users.noreply.github.com>
2023-04-21 17:11:30 +0900

    Update Minecraft_Linux_Fabric_Ver_1.0.0.0.sh

--- MineCraft/Minecraft_Linux_Fabric_Ver_1.0.0.0.sh
+++ MineCraft/Minecraft_Linux_Fabric_Ver_1.0.0.0.sh
@@ -49,15 +49,12 @@ read -r InstallerVersion
 MODAvailableCheck()
 #===============================================================================
 {
-  mkdir ~/minecraft
-  cd ~/minecraft/
   curl -OJ https://meta.fabricmc.net/v2/versions/loader/${MinecraftVersion}/${FabricVersion}/${InstallerVersion}/server/jar
   ls -l | grep fabric 
   
   if [ ${?} != "0" ]
   then
     echo "입력하신 버전으로 Fabric을 다운로드 할 수 없습니다. 버전 정보를 정확하게 다시 입력해주세요"
-    rm -rf ~/minecraft
     exit 1
   fi
 }
@@ -78,7 +75,6 @@ InstallApps()
 RunInstaller()
 #===============================================================================
 {
-  cd ~/minecraft
   java -jar ~/minecraft/fabric-server-mc.${MinecraftVersion}-loader.${FabricVersion}-launcher.${InstallerVersion}.jar nogui
 }
 
@@ -89,8 +85,7 @@ RunInstaller()
 EULA()
 #===============================================================================
 {
-cd ~/minecraft
-cat <<-EOF > ~/minecraft/eula.txt
+cat <<-EOF > ./eula.txt
 eula=true
 EOF
 }
@@ -161,7 +156,7 @@ echo "2. 메모리 크기는 환경에 맞춰서 수정하여 사용합니다. "
 echo "3. Fabric API는 https://www.curseforge.com/minecraft/mc-mods/fabric-api 에서 직접 다운로드 받아 mods 디렉토리에 업로드 합니다. "
 echo "4. 서버 커맨드 실행전 screen 명령어를 사용하여 백그라운드로 돌리세요"
 echo "========================================================================"
-echo "cd ~/minecraft"
+echo "서버 실행 명령어는 아래와 같습니다"
 echo "java -Xms2G -Xmx2G -jar ./fabric-server-mc.${MinecraftVersion}-loader.${FabricVersion}-launcher.${InstallerVersion}.jar nogui"
 echo
 echo
