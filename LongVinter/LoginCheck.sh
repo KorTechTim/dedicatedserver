ed88f75c92d0efd8e74460f3d36aa6d186b40096
KorTechTim <127585101+KorTechTim@users.noreply.github.com>
2023-06-17 08:03:04 +0900

    Update LoginCheck.sh

--- LongVinter/LoginCheck.sh
+++ LongVinter/LoginCheck.sh
@@ -4,11 +4,16 @@ while true
 do
 
     #===================================
-    # 변선 선언구 
+    # 사용자 선언구 
     #===================================
-    discord_webhook_url="Your_Webhook_URL_Here"
-    User_New=$(cat Your_Longvinter.log_File_Location_Here |grep "Join succeeded"|tail -n 1|dos2unix|cut -d 'd' -f3|cut -d ' ' -f2)
-    EOSID=$(cat Your_Longvinter.log_File_Location_Here |grep $User_New|grep Redpoint|tail -n 1|dos2unix|cut -d ':' -f6|cut -d ' ' -f1)
+    DiscordWebhookUrl=""
+    LogFileLocation=""
+
+    #===================================
+    # 프로그램 실행 구문
+    #===================================
+    User_New=$(cat ${LogFileLocation}/Longvinter.log |grep "Join succeeded"|tail -n 1|dos2unix|cut -d 'd' -f3|cut -d ' ' -f2)
+    EOSID=$(cat ${LogFileLocation}/Longvinter.log |grep $User_New|grep Redpoint|tail -n 1|dos2unix|cut -d ':' -f6|cut -d ' ' -f1)
     LogDate=`date "+%Y-%m-%d %H:%M:%S"`
     msg=$LogDate"    /    접속자: "$User_New"    /    EOSID : "$EOSID
 
@@ -17,12 +22,12 @@ do
         #==============================================
         # 새로운 사용자가 접속 했을 경우 (서버 로그 전송)
         #==============================================
-        curl -H 'Content-Type: application/json' -d '{"content": "'"${msg}"'"}' "${discord_webhook_url}"
+        curl -H 'Content-Type: application/json' -d '{"content": "'"${msg}"'"}' "${DiscordWebhookUrl}"
         User_Old=$User_New
 
     else
         #==============================================
-        # 새로운 사용자가 접속하지 않았을 때
+        # 새로운 사용자가 접속하지 않았을 때 (아무일 없음)
         #==============================================
         User_Old=$User_New
 
